6cecacab6507fb4cdd28986b87168cc32ebac942
Ismael Althenofer <ismaelalthenofer85@gmail.com>
2024-06-05 23:33:15 -0300

    Configuracion inicial del proyecto

---
+++ person-name/src/main/java/person_name/PersonNameApplication.java
@@ -0,0 +1,13 @@
+package person_name;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonNameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonNameApplication.class, args);
+	}
+
+}
